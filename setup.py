d11a1f377b92da73408306ad46a21d6fbc1c2ebb
DXsmiley <dxsmiley@hotmail.com>
2017-01-02 10:04:18 +1100

    Change formatting slightly

--- setup.py
+++ setup.py
@@ -5,10 +5,8 @@ setup(
 	version = '0.0.2',
 	description = 'C extention to check bounding box overlaps',
 	author = 'DXsmiley',
-	setup_requires=["cffi>=1.0.0"],
-	cffi_modules=["build.py:ffibuilder"],
-	install_requires=["cffi>=1.0.0"],
-	py_modules = [
-		'cbbox'
-	]
+	setup_requires = ["cffi>=1.0.0"],
+	cffi_modules = ["build.py:ffibuilder"],
+	install_requires = ["cffi>=1.0.0"],
+	py_modules = ['cbbox']
 )
